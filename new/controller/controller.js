7f6ac22362cb33e94b0c0470bf5d9698716a69c1
TSpg1012 <bd8g57ev2@code.edu.az>
2023-12-22 09:30:40 +0400

    modified

--- new/controller/controller.js
+++ new/controller/controller.js
@@ -17,6 +17,28 @@ const deleteKiwi = async (req, res) => {
   let deletedKiwi = await Seid.findByIdAndDelete(findKiwi._id);
 };
 
-const patchKiwi = async (req, res) => {};
+const patchKiwi = async (req, res) => {
+  let id = req.params.id;
+  let updated = await Seid.findOneAndUpdate({ id: id }, req.body);
+};
 
-module.exports = { getAllUser, getPostKiwi, deleteKiwi, patchKiwi };
+const getById = async (req, res) => {
+  let id = req.params.id;
+  let find = await Seid.findOne({ id: id });
+  // console.log(findUser);
+  res.send(find);
+};
+
+const getPutKiwi = async (req, res) => {
+  let id = req.params.id;
+  let updated = await Seid.replaceOne({ id: id }, req.body);
+};
+
+module.exports = {
+  getAllUser,
+  getPostKiwi,
+  deleteKiwi,
+  patchKiwi,
+  getById,
+  getPutKiwi,
+};
--- new/router/router.js
+++ new/router/router.js
@@ -5,5 +5,7 @@ router.get("/", controller.getAllUser);
 router.post("/", controller.getPostKiwi);
 router.delete("/:id", controller.deleteKiwi);
 router.patch("/:id", controller.patchKiwi);
+router.get("/:id", controller.getById);
+router.put("/:id", controller.getPutKiwi);
 
 module.exports = router;
